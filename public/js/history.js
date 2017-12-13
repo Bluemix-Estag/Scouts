f4c23ba8dd4be5ed26212c43f287f358d43784e2
gmarcon17 <guilhermemarcon12@gmail.com>
2017-12-13 17:39:19 -0200

    first commit

--- public/js/history.js
+++ public/js/history.js
@@ -4,10 +4,9 @@ try {
 } catch (err) {
     console.log("caquita do websocket");
 }
-
+var tempo= Date.now();
 
 ws.onopen = function (ev) {
-
     xhrGet('/players', function (resultado) {
         // aqui cai no caso de status code 200
         resultado = JSON.parse(resultado);
@@ -16,36 +15,34 @@ ws.onopen = function (ev) {
         var str = "";
         //var str = JSON.stringify(header);
         //console.log(document.getElementById('idDoHeader'));
-
         for (var i = 0; i < resultado[0].equipes[0].jogadores.length; i++) {
             str += createPlayerCard(resultado[0].equipes[0].jogadores[i], resultado[0].equipes[0].nome);
         }
         row.innerHTML += str;
-
     }, function (error) {
         //aqui cai no caso de erro
         alert('Ai caquita')
     })
 };
 
-ws.onclose = function (ev) {
-
-
-};
+ws.onclose = function (ev) {};
 
 ws.onmessage = function (ev) {
     console.log(ev.data);
     if (ev.data == "Updated") {
-        xhrGet('/players', function (resultado) {
+        xhrGet('/', function (resultado) {
+            // [ { nome: 'Neymar', tipo: 'certo', tempo: 'timestamp'} ];
             // aqui cai no caso de status code 200
             resultado = JSON.parse(resultado);
+            let temp_aux = resultado.tempo;
+            resultado.tempo = resultado.tempo - tempo;
+            tempo = resultado.temp_aux;
             var row = document.getElementById('idDoRow');
             var header = document.getElementById('foto').outerHTML + document.getElementById('idDoHeader').outerHTML;
             var str = header;
             //console.log(str);
-
-            for (var i = 0; i < resultado[0].equipes[0].jogadores.length; i++) {
-                str += createPlayerCard(resultado[0].equipes[0].jogadores[i], resultado[0].equipes[0].nome);
+            for (var i = 0; i < resultado.length; i++) {
+                str += createHistoryCard(resultado);
             }
             row.innerHTML = str;
 
@@ -54,23 +51,21 @@ ws.onmessage = function (ev) {
             alert('Ai caquita')
         })
     }
-
 };
 
-function createPlayerCard(player, team) {
+function createHistoryCard(scout) {
+    let nome = scout.name;
+    let lance = scout.tipo;
+    let tempo = scout.tempo;
     let img = (player.image != undefined) ? player.image : '/img/player_icon.png';
     let logo = (player.team_logo != undefined) ? player.team_logo : '/img/brasillogo.png';
     let role = (player.role != undefined) ? player.role : 'JOGADOR';
-    var overall = Math.round((player.scouts.certo * 0.3 + player.scouts.errado * (-0.3) + player.scouts.passe_decisivo * 5) * 100) / 100;
     return '<div class="col s12 m10 offset-m1 l8 offset-l2">' +
         '<div class="card row flex">' +
-        '<div class="col s1 nospace center-align"><img  src="' + logo + '"class="logo"/>' + team + '</div>' +
         '<div class="col s2 nospace center-align"><img  src="' + img + '" class="player-pic center-align nospace"/></div>' +
-        '<div class="col s1 nospace center-align"><span class="player_name">' + player.nome + '</span><br><span class="scouts-label">' + role + '</span></div>' +
-        '<div class="col s2 center-align scouts">' + overall + '</div>' +
-        '<div class="col s2 center-align scouts">' + player.scouts.certo + '</div>' +
-        '<div class="col s2 center-align scouts">' + player.scouts.errado + '</div>' +
-        '<div class="col s2 center-align scouts">' + player.scouts.passe_decisivo + '</div>' +
+        '<div class="col s1 nospace center-align"><span class="player_name">' + nome + '</span><br><span class="scouts-label">' + role + '</span></div>' +
+        '<div class="col s2 center-align scouts">' + lance + '</div>' +
+        '<div class="col s2 center-align scouts">' + tempo + '</div>' +
         '</div>' +
         '</div>';
 }
@@ -103,7 +98,6 @@ function xhrGet(url, callback, errback) {
             }
         }
     };
-
     xhr.timeout = 100000;
     xhr.ontimeout = errback;
     xhr.send();

8402c083aa453f73a5467da851ac8bb08376e03b
gmarcon17 <guilhermemarcon12@gmail.com>
2017-12-14 14:47:45 -0200

    timestamp

--- public/js/index.js
+++ public/js/index.js
@@ -1,77 +1,78 @@
 var wsUri = "wss://jimmycricket-orquestrador.mybluemix.net/websocket";
 try {
     var ws = new WebSocket(wsUri);
-} catch (err) {
+}catch(err){
     console.log("caquita do websocket");
 }
 
 
-ws.onopen = function (ev) {
-
-    xhrGet('/players', function (resultado) {
+ws.onopen = function(ev) {
+    
+    xhrGet('/players', function(resultado) {
         // aqui cai no caso de status code 200
-        resultado = JSON.parse(resultado);
-        var row = document.getElementById('idDoRow');
-        //var header = document.getElementById('idDoHeader').outerHTML;
-        var str = "";
-        //var str = JSON.stringify(header);
-        //console.log(document.getElementById('idDoHeader'));
-
-        for (var i = 0; i < resultado[0].equipes[0].jogadores.length; i++) {
-            str += createPlayerCard(resultado[0].equipes[0].jogadores[i], resultado[0].equipes[0].nome);
-        }
-        row.innerHTML += str;
-
-    }, function (error) {
-        //aqui cai no caso de erro
-        alert('Ai caquita')
-    })
-};
-
-ws.onclose = function (ev) {
-
-
-};
-
-ws.onmessage = function (ev) {
-    console.log(ev.data);
-    if (ev.data == "Updated") {
-        xhrGet('/players', function (resultado) {
-            // aqui cai no caso de status code 200
             resultado = JSON.parse(resultado);
             var row = document.getElementById('idDoRow');
-            var header = document.getElementById('foto').outerHTML + document.getElementById('idDoHeader').outerHTML;
-            var str = header;
-            //console.log(str);
-
-            for (var i = 0; i < resultado[0].equipes[0].jogadores.length; i++) {
+            //var header = document.getElementById('idDoHeader').outerHTML;
+            var str = "";
+            //var str = JSON.stringify(header);
+            //console.log(document.getElementById('idDoHeader'));
+        
+            for(var i = 0 ; i < resultado[0].equipes[0].jogadores.length; i++){
                 str += createPlayerCard(resultado[0].equipes[0].jogadores[i], resultado[0].equipes[0].nome);
             }
-            row.innerHTML = str;
-
+            row.innerHTML += str;
+        
         }, function (error) {
             //aqui cai no caso de erro
             alert('Ai caquita')
-        })
-    }
+    })
+};
+
+ws.onclose = function(ev) {
+
 
 };
 
-function createPlayerCard(player, team) {
-    let img = (player.image != undefined) ? player.image : '/img/player_icon.png';
-    let logo = (player.team_logo != undefined) ? player.team_logo : '/img/brasillogo.png';
-    let role = (player.role != undefined) ? player.role : 'JOGADOR';
-    var overall = Math.round((player.scouts.certo * 0.3 + player.scouts.errado * (-0.3) + player.scouts.passe_decisivo * 5) * 100) / 100;
-    return '<div class="col s12 m10 offset-m1 l8 offset-l2">' +
-        '<div class="card row flex">' +
-        '<div class="col s1 nospace center-align"><img  src="' + logo + '"class="logo"/>' + team + '</div>' +
-        '<div class="col s2 nospace center-align"><img  src="' + img + '" class="player-pic center-align nospace"/></div>' +
-        '<div class="col s1 nospace center-align"><span class="player_name">' + player.nome + '</span><br><span class="scouts-label">' + role + '</span></div>' +
-        '<div class="col s2 center-align scouts">' + overall + '</div>' +
-        '<div class="col s2 center-align scouts">' + player.scouts.certo + '</div>' +
-        '<div class="col s2 center-align scouts">' + player.scouts.errado + '</div>' +
-        '<div class="col s2 center-align scouts">' + player.scouts.passe_decisivo + '</div>' +
-        '</div>' +
+ws.onmessage = function(ev) {
+    console.log(ev.data);
+    if(ev.data == "Updated"){
+        xhrGet('/players', function(resultado) {
+            // aqui cai no caso de status code 200
+                resultado = JSON.parse(resultado);
+                var row = document.getElementById('idDoRow');
+                var header = document.getElementById('foto').outerHTML+document.getElementById('idDoHeader').outerHTML;
+                var str = header;
+                //console.log(str);
+            
+                for(var i = 0 ; i < resultado[0].equipes[0].jogadores.length; i++){
+                    str += createPlayerCard(resultado[0].equipes[0].jogadores[i], resultado[0].equipes[0].nome);
+                }
+                row.innerHTML = str;
+            
+            }, function (error) {
+                //aqui cai no caso de erro
+                alert('Ai caquita')
+        })
+    }
+    
+};
+
+
+function createPlayerCard(player, team){
+    let img = (player.image != undefined )?player.image:'/img/player_icon.png';
+    let logo = (player.team_logo != undefined )?player.team_logo:'/img/brasillogo.png';
+    let role = (player.role != undefined )?player.role:'JOGADOR';
+    var overall = Math.round((player.scouts.certo*0.3+player.scouts.errado*(-0.3)+player.scouts.passe_decisivo*5)*100)/100;
+    return '<div class="col s12 m10 offset-m1 l8 offset-l2">'+
+            '<div class="card row flex">'+
+                    '<div class="col s1 nospace center-align"><img  src="'+logo+'"class="logo"/>'+team+'</div>'+
+                    '<div class="col s2 nospace center-align"><img  src="'+img+'" class="player-pic center-align nospace"/></div>'+
+                    '<div class="col s1 nospace center-align"><span class="player_name">'+player.nome+'</span><br><span class="scouts-label">'+role+'</span></div>'+
+                    '<div class="col s2 center-align scouts">'+overall+'</div>'+
+                    '<div class="col s2 center-align scouts">'+player.scouts.certo+'</div>'+
+                    '<div class="col s2 center-align scouts">'+player.scouts.errado+'</div>'+
+                    '<div class="col s2 center-align scouts">'+player.scouts.passe_decisivo+'</div>'+                
+           '</div>'+
         '</div>';
 }
 
@@ -84,12 +85,11 @@ function createXHR() {
         } catch (e) {
             try {
                 return new ActiveXObject('Microsoft.XMLHTTP');
-            } catch (e) {}
+            } catch (e) { }
         }
     }
     return null;
 }
-
 function xhrGet(url, callback, errback) {
     var xhr = new createXHR();
     xhr.open("GET", url, true);
@@ -104,22 +104,7 @@ function xhrGet(url, callback, errback) {
         }
     };
 
-    xhr.timeout = 100000;
+   xhr.timeout = 100000;
     xhr.ontimeout = errback;
     xhr.send();
-}
-
-function createHistoryCard(player, team) {
-    let img = (player.image != undefined) ? player.image : '/img/player_icon.png';
-    let logo = (player.team_logo != undefined) ? player.team_logo : '/img/brasillogo.png';
-    let role = (player.role != undefined) ? player.role : 'JOGADOR';
-    return '<div class="col s6 offset-7">' +
-        '<div class="card row flex">' +
-        '<div class="col s3 scouts-label center-align"><img  src="' + logo + '"class="logo"/>' + team + '</div>' +
-        '<div class="col s3 scouts-label center-align"><img  src="' + img + '" class="player-pic center-align nospace"/></div>' +
-        '<div class="col s3 scouts-label center-align">' + player.nome + '</span><br><span class="scouts-label">' + role + '</span></div>' +
-        '<div class="col s3 scouts-label center-align">' + player.scouts.lance  + '</div>' +
-        '<div class="col s3 scouts-label center-align">' + /*QUESTAO DO TEMPO*/ + '</div>' +
-        '</div>' +
-        '</div>';
 }
